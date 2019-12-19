9105041b1b85338285103f508423f81e741f93bc
Kalvin Low <ke2low@uwaterloo.ca>
2019-12-19 20:39:16 +0000

    used eslint to clean up formatting errors on composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
-// counts amount of text inputted into form and displays 
+// counts amount of text inputted into form and displays
 // how many remaining characters are left to be used
 $(document).ready(function() {
   $('.new-tweet form textarea').on('input', function() {
     let maxLength = 140;
     let currentLength = $(this).val().length;
-    let count = maxLength - currentLength; 
+    let count = maxLength - currentLength;
     if (count < 0)  {
       $('.counter').text(count);
       $('.counter').addClass("warning");
