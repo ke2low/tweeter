a55b74257f4371e0f0bb54eb97c65900806f5c10
Kalvin Low <ke2low@uwaterloo.ca>
2019-12-19 20:30:59 +0000

    made website responsive

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+// counts amount of text inputted into form and displays 
+// how many remaining characters are left to be used
+$(document).ready(function() {
+  $('.new-tweet form textarea').on('input', function() {
+    let maxLength = 140;
+    let currentLength = $(this).val().length;
+    let count = maxLength - currentLength; 
+    if (count < 0)  {
+      $('.counter').text(count);
+      $('.counter').addClass("warning");
+    } else if (count >= 0) {
+      $('.counter').removeClass("warning");
+      $('.counter').text(count);
+    }
+  });
+});
